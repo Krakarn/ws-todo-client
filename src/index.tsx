35e84800de51fb2733145d7a6a74a335af37e667
Krakarn <linus.engelbrektsson@gmail.com>
2017-10-25 00:43:02 +0200

    -

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { QueueingSubject } from 'queueing-subject';
-import websocketConnect from 'rxjs-websockets';
+import { App } from './components/app';
+import { AppState } from './state/app';
 
-const input = new QueueingSubject<string>();
-
-const { messages, connectionStatus } = websocketConnect('ws://localhost:8090', input);
-
-input.next('hello server!');
-
-const connectionStatusSubscription = connectionStatus.subscribe(numberConnected => {
-  console.log('Number of connected websockets:', numberConnected);
-});
-
-const messagesSubscription = messages.subscribe(message => console.log('Server message:', message));
-
-class App extends React.Component {
-  public render() {
-    return <p>Hello, world!</p>;
-  }
-}
+const app = new AppState();
 
 const container = document.createElement('div');
 document.body.appendChild(container);
 
-ReactDOM.render(<App />, container);
+ReactDOM.render(<App app={app} />, container);
